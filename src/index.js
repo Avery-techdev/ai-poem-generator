32a19e874bac8744107e2f0d77d8759c1afb1dd4
Avery Hauschild </>
2023-12-21 16:07:03 +0100

    added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
+function displayPoem(response) {
+  new Typewriter("#output", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+} 
+  function generatePoem(event) {
     event.preventDefault();
   
-    new Typewriter("#output", {
-      strings: "In shadows soft, where moonlight gleams Whiskered whispers, in midnight dreams. Graceful prowlers, velvet paws, Cats dance silently, nature's applause.",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "fa02684f7db0feb365o91eta3c0acd12";
+    let context =
+      "You are a Poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions. Do not include a title to the poem.";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+    let poemElement = document.querySelector("#output");
+    // poemElement.classList.remove("hidden");
+    //poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayPoem);
   }
   
   let poemFormElement = document.querySelector("#input");
