e29dd2ff43cb39f031668740373827bf33947bd4
Avery Hauschild </>
2023-12-22 15:48:11 +0100

    improved user experience

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ function displayPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
-    cursor: "",
   });
-} 
-  function generatePoem(event) {
-    event.preventDefault();
-  
-    let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey = "fa02684f7db0feb365o91eta3c0acd12";
-    let context =
-      "You are a Poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions. Dont show HTML tags and use for every line a new line with 1.5 line height";
-    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
-    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  
-    let poemElement = document.querySelector("#output");
-    // poemElement.classList.remove("hidden");
-    //poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
-  
-    axios.get(apiURL).then(displayPoem);
-  }
-  
-  let poemFormElement = document.querySelector("#input");
-  poemFormElement.addEventListener("submit", generatePoem);
+}
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "fa02684f7db0feb365o91eta3c0acd12";
+  let context =
+    "You are a Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. ";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#output");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏱️ Generating a poem about ${instructionsInput.value}...</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#input");
+poemFormElement.addEventListener("submit", generatePoem);
